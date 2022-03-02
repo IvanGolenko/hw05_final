ba59b24d90ea1b1ce04fafbe8963843c82fd96f4
Иван Голенко <igolivanenko1996@gmail.com>
2022-03-02 22:27:48 +0300

    Спринт 6. Попытка 2

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     # Переменная exception содержит отладочную информацию;
     # выводить её в шаблон пользовательской страницы 404 мы не станем
-    return render(request, 'core/404.html',
+    return render(
+        request, 'core/404.html',
         {'path': request.path}, status=HTTPStatus.NOT_FOUND)
 
 
@@ -19,5 +20,6 @@ def permission_denied_view(request, exception):
 
 
 def server_eror(request):
-    return render(request, 'core/500.html',
+    return render(
+        request, 'core/500.html',
         status=HTTPStatus.INTERNAL_SERVER_ERROR)
